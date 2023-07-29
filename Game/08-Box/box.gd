3c15e69e794731ea7ad0b06fb782b13f902c1a5c
Eternal1422 <eternal1422@gmail.com>
2023-07-29 19:24:44 -0400

    Destroyable Boxes
    Make boxes able to be destroyed when attacked.

---
+++ Game/08-Box/box.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+var in_attack_hitbox = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if in_attack_hitbox and Global.enemy_in_range and Global.currently_attacking:
+		queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	in_attack_hitbox = true
+
+
+func _on_area_2d_area_exited(area):
+	in_attack_hitbox = false
