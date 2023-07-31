d52e648200abf898ed42f190cc52fbb980b7c3cc
Eternal1422 <eternal1422@gmail.com>
2023-07-30 20:00:34 -0400

    Fixed boxes breaking randomly.
    Boxes were on "Enemies" layer and they their collisions were targetingWorld, Enemy and Non-Targetagbles.

--- Game/08-Box/box.gd
+++ Game/08-Box/box.gd
@@ -6,6 +6,7 @@ var in_attack_hitbox = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if in_attack_hitbox and Global.enemy_in_range and Global.currently_attacking:
+		print(in_attack_hitbox)
 		queue_free()
 
 
