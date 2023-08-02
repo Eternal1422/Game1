510f31a53f4a13878f44a8567383c825bc01286b
Eternal1422 <eternal1422@gmail.com>
2023-08-02 17:05:50 -0400

    Next Level + Boss Level
    Made a next level button and the boss level fight room

--- Game/World/world.gd
+++ Game/World/world.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@export var next_level: PackedScene
 
 func _ready():
+	on_level_completed()
 	Global.level_completed.connect(on_level_completed)
 	
 func on_level_completed():
 	level_completed.show()
-	get_tree().paused = true
-
-
-	
+ 
+func _on_level_completed_next_level():
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
